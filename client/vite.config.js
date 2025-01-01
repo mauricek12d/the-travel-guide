ccc9904b48d3b605535c89b708b58d9841ca5807
Maurice Zuniga <mauricek12d@gmail.com>
2024-12-31 20:11:43 -0500

    change the envDIR on the Vite Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: '.',
+  plugins: [react()],
   server: {
+    cache: false,
     port: 3000,
     open: true,
     proxy: {
@@ -10,6 +14,11 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
+      },
+      '/weather': {
+        target: 'https://api.openweathermap.org/data/2.5',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/weather/, ''),
       },
     },
   },
